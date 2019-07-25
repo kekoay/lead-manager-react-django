8a88f8dea8d7b8df55a72d56f992a9171a087c01
kekoay <kekoay@gmail.com>
2019-07-25 06:12:32 -0400

    Added the basic frontend to project

---
+++ leadsmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Leads extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Leads list</h1>
+            </div>
+        );
+    }
+
+}
+
+export default Leads;
